dfb558b6f65f5a4bda31b5fcc500ba3ddb4251fd
Nooruddin Khorasi <nooruddin.khorasi@gmail.com>
2019-02-26 18:35:50 -0800

    some changes to appjs

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import IEXSymbols from "./components/IEXSymbols";
 
 //  all queries are accessed by this url and graphql does the heavy lifting from there.
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
+  uri: "/graphql"
 });
 
 class App extends Component {
